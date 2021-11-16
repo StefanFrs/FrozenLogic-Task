d9d5ff7fad2dd16930ba8c90555faae919d302eb
StefanFrs <61791695+StefanFrs@users.noreply.github.com>
2021-11-16 14:39:55 +0200

    HTML, CSS + JS added

---
+++ script.js
@@ -0,0 +1,12 @@
+var mybutton = document.getElementById("scrollBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
