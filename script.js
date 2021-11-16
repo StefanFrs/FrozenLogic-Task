59a5ff6ec9f88504517a9e21202adafbd152ddaf
StefanFrs <61791695+StefanFrs@users.noreply.github.com>
2021-11-16 15:18:15 +0200

    remove commented code

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 var mybutton = document.getElementById("scrollBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
